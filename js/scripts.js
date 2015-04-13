b5aa3f9e78b56ea2ea3165e0929b23c8a7a66d3b
james nielson & Kyle Bulloch <student@epicodus.com>
2015-04-13 11:45:29 -0700

    Add jquery to submit form and show list

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,16 @@
 $(document).ready(function(){
-  
+  $('form#place_name').focus();
+  $('form#place-form').submit(function(event) {
+    var placeName = $('input#place_name').val();
+    var season = $('input#season').val();
+    var review = $('textarea#review').val();
+    var trip = {placeName: placeName, season: season, review: review };
+
+    $('ul#place-list').append("<li><span class='place'>" + trip.placeName + "</span></li>");
+
+
+    $('#place-list').show();
+    event.preventDefault();
+  });
 
 });
